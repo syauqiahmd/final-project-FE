3befba7fa1b09cc27282b3f11b7feb7eefbb1dce
awressy <awressy@gmail.com>
2022-12-01 21:28:50 +0700

    add footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,16 +1,22 @@
 import { Link, useNavigate } from "react-router-dom"
 import { useSelector, useDispatch } from 'react-redux'
 import ImgLogoWhite from '../assets/logo-putih.png'
-import { FontAwesomeIcon } from 'react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 export default function Footer(){
 	return (
 		<footer id="footer">
 			<div className="container d-flex flex-column justify-content-center align-items-center">
-				<img src={ImgLogoWhite} />
+				<img src={ImgLogoWhite} className="mb-4" />
 				<span>+62 896-8710-1195</span>
-				<span>Indonesia</span>
-				<span><FontAwesomeIcon icon="fa-solid fa-house" /></span>
+				<span className="mb-4">Indonesia</span>
+				<span className="icon-groups d-flex justify-content-between">
+					<FontAwesomeIcon icon="fa-brands fa-facebook" />
+					<FontAwesomeIcon icon="fa-brands fa-twitter" />
+					<FontAwesomeIcon icon="fa-brands fa-github" />
+					<FontAwesomeIcon icon="fa-brands fa-linkedin" />
+				</span>
 			</div>
 		</footer>
 	)
