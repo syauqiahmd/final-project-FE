32b2b9fa2f6a184c782454d702a9ce697b4a93ab
awressy <awressy@gmail.com>
2022-12-02 13:36:39 +0700

    edit layout with footer

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom"
 import Navigation from './Navigation'
+import Footer from "../components/Footer";
+
 export default function Layout(){
 	return (
 		<>
 			<Navigation />
 			<Outlet />
+			<Footer />
 		</>
 	)
 }
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -6,7 +6,6 @@ import { Button } from 'react-bootstrap';
 import Slideshow from "../components/Slideshow";
 import TopProject from '../components/TopProject'
 import About from "../components/About";
-import Footer from "../components/Footer";
 
 export default function Home(){
 	return (
@@ -14,28 +13,6 @@ export default function Home(){
             <Slideshow />
             <TopProject />
             <About />
-            <Footer />
-
-            {/* <Button variant="primary">Primary</Button>{' '}
-            <Button variant="secondary">Secondary</Button>{' '}
-            <Button variant="success">Success</Button>{' '}
-            <Button variant="warning">Warning</Button>{' '}
-            <Button variant="danger">Danger</Button>{' '}
-            <Button variant="info">Info</Button>{' '}
-            <Button variant="light">Light</Button>{' '}
-            <header className="App-header">
-                <p>
-                Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                Learn React
-                </a>
-            </header> */}
         </>
 	)
 }
