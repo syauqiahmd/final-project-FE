c2748eaab8540302e86d3cce8a1dd85623943b81
awressy <awressy@gmail.com>
2022-12-03 06:47:28 +0700

    navigation delete comments

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -16,31 +16,6 @@ export default function Navigation(){
 		}
 	}
 	return (
-		// <header >
-		// 	<nav className="d-flex justify-content-around align-items-center navbar navbar-expand-md">
-		// 		<span className="navbar-brand">
-		// 			<img src={ImgLogo} />
-					
-		// 		</span>
-
-		// 		<a onClick={appear} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-		// 			<span className="navbar-toggler-icon"></span>
-		// 		</a>
-
-		// 		<span className={collpase + " navbar-collapse"} id="navbarNavAltMarkup">
-		// 			<span className="navbar-nav">
-		// 				<a className="nav-item nav-link">Home</a>
-		// 				<a className="nav-item nav-link">Project</a>
-		// 				<a className="nav-item nav-link">About</a>
-		// 			</span>
-		// 		</span>
-
-		// 		<span className={collpase + " navbar-collapse"} id="navbarNavAltMarkup">
-		// 			<a className="btn btn-outline-dark">Register</a>
-		// 			<a className="btn btn-outline-dark">Login</a>
-		// 		</span>
-		// 	</nav>
-		// </header>
 		<nav className="navbar navbar-expand-md d-flex justify-content-around align-items-center">
 			<span>
 				<img src={ImgLogo} />
---
+++ client/src/views/Project.jsx
@@ -0,0 +1,17 @@
+import { Link, useNavigate } from "react-router-dom"
+import { useSelector, useDispatch } from 'react-redux'
+
+export default function Project(){
+	const cardTotal = 8
+	return (
+		<>
+			<div id="project">
+				<div className="container">
+					{
+
+					}
+				</div>
+			</div>
+		</>
+	)
+}
