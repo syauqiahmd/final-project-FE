0b6dcd0ed4fbc0fc5dd3e0e23655a74ff2996919
Jonathan <jrs01210@gmail.com>
2022-12-03 11:58:13 +0700

    added bootstrap, template register

--- admin-cms/src/views/RegisterForm.jsx
+++ admin-cms/src/views/RegisterForm.jsx
@@ -1,4 +1,5 @@
-const { useState } = require("react");
+import { useState } from "react";
+import { Button, Col, Form, Row, Card } from "react-bootstrap";
 
 export default function Register() {
   const [formUser, setFormUser] = useState({
@@ -21,7 +22,37 @@ export default function Register() {
 
   return (
     <>
-      <h1>Register Page</h1>
+      <Card style={{  width: "50%", padding: "2px"}} className="justify-content-around">
+        <Card.Title>Register Admin</Card.Title>
+        <Form>
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridFN">
+              <Form.Label className="text-left">Full Name</Form.Label>
+              <Form.Control placeholder="Enter Full Name" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridUN">
+              <Form.Label>Username</Form.Label>
+              <Form.Control placeholder="Enter Username" />
+            </Form.Group>
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+          </Row>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Card>
     </>
   );
 }
