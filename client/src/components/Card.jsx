38f12b692d8157a361f3ba85de0ea75428c8219f
Ahmad Syauqi <syauqiahmd@Ahmads-MacBook-Pro.local>
2022-12-07 16:38:04 +0800

    style(card): project card modified

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -10,21 +10,20 @@ export default function Card(props) {
   };
   // set full height card to be same with other card
   return (
-    <div className="card mb-4">
-      <Button
-        to="/project/1"
-        className="btn btn-light"
-        style={{ border: "none", backgroundColor: "transparent", padding: 0 }}
-        onClick={handlerProjectId}>
+    <div className="card h-100"
+      to="/project/1"
+      style={{ border: "none", padding: 0 }}
+      onClick={handlerProjectId}
+    >
         <img className="card-img-top" src={props.data?.imgUrl} alt="Card image cap" />
         <div className="card-body">
-          <h4 className="card-title">{props.data?.title}</h4>
-          <span>{props.data?.Tag.name}</span>
-          <br />
-          <span>{props.data?.User.username}</span>
-          <div className="text-center"></div>
+          <div className="card-title">{props.data?.title}</div>
         </div>
-      </Button>
+        <div className="float-left px-3 mb-2">
+        <span class="badge rounded-pill text-bg-danger">{props.data?.Tag.name}</span>
+          <br />
+          <span><small>Posted By : <b>{props.data?.User.username}</b> </small></span>
+        </div>
     </div>
   );
 }
